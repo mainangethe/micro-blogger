349e2657efdf2536374d940f6678ab085c8ec6b3
Ng'ethe Maina <ngethe@mradi.co.ke>
2019-03-31 18:30:09 +0300

    Add `.tweet` method
    This method allows a user to post to Twitter so long as the message is not more than 140 characters.
    
    JIRA: BEDT-232

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -7,4 +7,12 @@ class MicroBlogger
     puts "Initializing.."
     @client = JumpstartAuth.twitter
   end
+
+  def tweet message
+    if message.length > 140
+      puts "WARNING: Please reenter the message again with less than 140 characters."
+    else
+      @client.update message
+    end
+  end
 end
