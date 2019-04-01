306e46f52ed812ecff90e824a22fe3587dcea164
Ng'ethe Maina <ngethe@mradi.co.ke>
2019-04-01 11:35:11 +0300

    Add Tweet with URL feature
    Add command to shorten a url before tweeting it.
    
    JIRA: BEDT-232

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -82,6 +82,7 @@ class MicroBlogger
       when 'spam' then spam_my_followers parts[1..-1].join(' ')
       when 'elt' then everyones_last_tweet
       when 's' then shorten parts[1]
+      when 'turl' then tweet(parts[1..-2].join(' ') + " " + shorten(parts[-1]))
       else
         puts "Sorry, I don't know how to deal with (#{ command })"
       end
