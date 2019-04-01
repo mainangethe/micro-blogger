9e86abe1df8d74a7b3ec6ad88ff041942f8b0a94
Ng'ethe Maina <ngethe@mradi.co.ke>
2019-04-01 10:47:08 +0300

    Add Direct Messaging functions
    There are two(2) functions for this feature.
    - `#dm` : allows a user to send a message to an individual target
    - `#spam_my_followers`: sends a message to my followers list
    
    JIRA: BEDT-232

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -16,6 +16,33 @@ class MicroBlogger
     end
   end
 
+  def dm target, message
+    screen_names = @client.followers.collect { |follower| @client.user(follower).screen_name }
+    if screen_names.include? target
+      puts "Trying to send #{ target } this direct message."
+      puts message
+      tweet_message = "d @#{ target } #{ message }"
+      tweet tweet_message
+    else
+      puts "WARNING: Please resend a dm to people who follow your account."
+    end
+  end
+
+  def followers_list
+    screen_names = []
+    @client.followers.collect { |follower| screen_names << @client.user(follower).screen_name }
+    screen_names
+  end
+
+  def spam_my_followers message
+    my_followers = followers_list
+    my_followers.each do |follower|
+      dm  follower, message
+    end
+  end
+
+
+
   def run
     puts "Welcome to Mradi's Twitter Client!"
     command = ""
@@ -28,6 +55,8 @@ class MicroBlogger
       case command
       when 'q' then puts "Goodbye! Until next time"
       when 't' then tweet parts[1..-1].join(' ')
+      when 'dm' then dm(parts[1], parts[2..-1].join(' '))
+      when 'spam' then spam_my_followers parts[1..-1].join(' ')
       else
         puts "Sorry, I don't know how to deal with (#{ command })"
       end
@@ -36,4 +65,5 @@ class MicroBlogger
 end
 
 blogger = MicroBlogger.new
+blogger.followers_list
 blogger.run
